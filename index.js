1ad69d8ff7bde482e35ffb2830b7fac8527847be
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-05-02 11:10:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const app = "I don't do much.";
+const token = "token went here"
+fetch("https://api.github.com/user/repos", {
+  headers: { Authorization: `token ${token}`}
+}).then(res => res.json()).then(json => console.log(json));
